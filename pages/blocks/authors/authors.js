c02b5b115d72769c1f85b6a7b685264f4c9e0740
Solène Duprat <solene.duprat@gmail.com>
2023-03-13 14:24:37 -0400

    [cleanup]: import createTag and replaceKey from milo instead.

--- pages/blocks/authors/authors.js
+++ pages/blocks/authors/authors.js
@@ -1,14 +1,17 @@
-import { getMetadata, createTag, fetchPlaceholders } from '../../scripts/utils.js';
+import { 
+  getMetadata, 
+  createTag,
+  getConfig,
+  replaceKey,
+  toSentenceCase
+} from '../../scripts/utils.js';
 
 export default async function authors(block) {
   const metaAuthors = getMetadata('authors');
-  const placeholders = await fetchPlaceholders((placeholders) => placeholders);
-
   const heading = createTag('h3');
   const authors = createTag('p');
-
-  heading.textContent = `${placeholders['authors-block-heading']}:`;
+  const headingTitle = await replaceKey('authors', getConfig());
+  heading.textContent = toSentenceCase(headingTitle);
   authors.textContent = metaAuthors;
-
   block.append(heading, authors);
 }
